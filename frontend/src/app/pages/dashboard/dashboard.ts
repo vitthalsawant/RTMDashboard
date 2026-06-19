a2472b448f9382a3226f2e858b9e8492175130b4
vitthalsawant <vitthalsawant300@gmail.com>
2026-06-19 15:07:28 +0530

    charts cards

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { KpiCards } from '../../components/kpi-cards/kpi-cards';
+import { BarChart } from '../../components/bar-chart/bar-chart';
+import { TrendChart } from '../../components/trend-chart/trend-chart';
+import { PieChart } from '../../components/pie-chart/pie-chart';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Header, KpiCards],
+  imports: [Header, KpiCards, BarChart, TrendChart, PieChart],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
